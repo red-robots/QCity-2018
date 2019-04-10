5e1ff1478a38139e76842185a2e11490825d65a4
Austin Crane <austincrane@gmail.com>
2019-04-10 12:13:41 -0400

    membership page

---
+++ page-templates/page-membership.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * Template Name: Membership
+ */
+acf_form_head();
+// sanitize inputs
+add_filter('acf/update_value', 'wp_kses_post', 10, 1);
+
+get_header(); 
+
+$banner_image = get_field("banner_photof");
+$video = get_field("video");
+$description = get_field("description");
+$tier_1_desc = get_field("tier_1_desc");
+$tier_2_desc = get_field("tier_2_desc");
+$tier_3_desc = get_field("tier_3_desc");
+?>
+
+	<div id="primary" class="">
+		
+		<div class="jobs-banner">
+			<?php if($banner_image) echo '<div class="background" style="background-image: url('.$banner_image['url'].');"></div>';?>
+			<?php if($banner_copy):?>
+				<div class="row-1">
+					<?php echo $banner_copy;?>
+				</div><!--.row-1-->
+			<?php endif;?>
+			
+		</div><!--.jobs-banner-->
+		<div id="content" role="main" class="wrapper">
+
+			<?php while ( have_posts() ) : the_post();?>
+				<div class="site-content-full">
+					<header class="archive-header">
+						<div class="border-title">
+							<h1><?php the_title(); ?></h1>
+						</div><!-- border title -->
+					</header><!-- .archive-header -->
+					
+					<section class="member-intro">
+						<?php if( $video ) { ?>
+							<div class="video">
+								<div class="embed-container">
+									<?php the_field('video'); ?>
+								</div>
+							</div>		
+						<?php } ?>
+						<div class="entry-content">
+							<?php echo $description; ?>
+						</div><!-- entry-content -->
+					</section>
+					
+					<section class="tiers membership-thirds">
+						<div class="third entry-content">
+							<?php echo $tier_1_desc; ?>
+							<div class="button">
+								<a class="gallery" href="">
+									Purchase Tier 1
+								</a>
+							</div>	
+						</div>
+						<div class="third entry-content">
+							<?php echo $tier_2_desc; ?>
+							<div class="button">
+								<a class="gallery" href="">
+									Purchase Tier 2
+								</a>
+							</div>	
+						</div>
+						<div class="third entry-content">
+							<?php echo $tier_3_desc; ?>
+							<div class="button">
+								<a class="gallery" href="">
+									Purchase Tier 3
+								</a>
+							</div>	
+						</div>
+					</section>
+				</div><!--.site-content-->
+				
+			<?php endwhile; // end of the loop. ?>
+		</div><!-- #content -->
+	</div><!-- #primary -->
+<?php get_footer(); ?>
